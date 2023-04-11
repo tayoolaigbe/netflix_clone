d5eecf3dd67372bc660c9cf658f92d4c8d717cbf
tayoolaigbe <tayoolaigbe@outlook.com>
2023-04-11 15:32:03 +0100

    Completed the Navigation bar and added some effect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { NextPageContext } from 'next';
 import { getSession, signOut } from 'next-auth/react';
 import useCurrentUser from '../hooks/useCurrentUser';
+import Navbar from '../components/Navbar';
 
 export async function getServerSideProps(context: NextPageContext) {
 	const session = await getSession(context);
@@ -23,11 +24,7 @@ export default function Home() {
 	const { data: user } = useCurrentUser();
 	return (
 		<>
-			<h1>Netflix Clone</h1>
-			<p className="text-white">Logged in as: {user?.name}</p>
-			<button className="h-10 w-full bg-white" onClick={() => signOut()}>
-				Log Out
-			</button>
+			<Navbar />
 		</>
 	);
 }
