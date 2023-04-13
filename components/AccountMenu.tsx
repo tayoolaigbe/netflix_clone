da7fe24fab4620cfecbf240bfd6516b54dcec466
tayoolaigbe <tayoolaigbe@outlook.com>
2023-04-13 21:45:59 +0100

    Modified AccountMenu to display correct username

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,10 +1,12 @@
 import { signOut } from 'next-auth/react';
+import useCurrentUser from '../hooks/useCurrentUser';
 
 interface AccountMenuProps {
 	visible?: boolean;
 }
 
 const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
+	const { data } = useCurrentUser();
 	if (!visible) {
 		return null;
 	}
@@ -18,7 +20,7 @@ const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
 						alt=""
 					/>
 					<p className="text-white text-sm group-hover/item:underline">
-						Username
+						{data?.name}
 					</p>
 				</div>
 				<hr className="bg-gray-600 border-0 h-px my-4" />
